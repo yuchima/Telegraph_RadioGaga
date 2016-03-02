54dcb21099948eca0c88c889b9eb3380157076f5
yuchima <ym1356@nyu.edu.com>
2016-03-02 17:49:28 -0500

    add arduino

---
+++ Arduino_RadioGaGa/Arduino_RadioGaGa.ino
@@ -0,0 +1,27 @@
+
+int key = 7;  // Pin 4 and using it as digital
+int speaker = 4; // Speaker will be hooked between pin 11 and ground
+
+int val = 0;       // A value for key up and down
+int 
+
+int myTone = 4000;  // Freq. of our tone
+
+void setup() {
+  pinMode(key, INPUT);
+  pinMode(speaker, OUTPUT);
+}
+
+void loop() {
+  val = digitalRead(key);        // Read the state of the key
+  if (val) {
+    // If it is down play the tone
+    tone(speaker, myTone);
+  }
+
+  if (!val) {
+    // If it is not down stop the tone
+    noTone(speaker);
+  }
+}
+
