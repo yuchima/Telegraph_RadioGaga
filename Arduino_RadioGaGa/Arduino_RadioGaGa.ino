302427f4cc4d2e63245e28f8598ae91904311dc9
yuchima <ym1356@nyu.edu.com>
2016-03-02 20:36:09 -0500

    output matching

--- Arduino_RadioGaGa/Arduino_RadioGaGa.ino
+++ Arduino_RadioGaGa/Arduino_RadioGaGa.ino
@@ -1,100 +1,79 @@
-const byte inPin = 7;
+/**
+ * morse-code.ino
+ *
+ * Author: Ryan Jacobs <ryan.mjacobs@gmail.com>
+ * Author: Kyle Domen <kdomen99@gmail.com>
+ */
 
-//letters (0-25), digits (26-36)
-const byte MORSECODE [][5] = {{1, 2, 0, 0, 0}, {2, 1, 1, 1, 0}, {2, 1, 2, 1, 0}, {2, 1, 1, 0, 0}, {1, 0, 0, 0, 0}, {1, 1, 2, 1, 0}
-  , {2, 2, 1, 0, 0}, {1, 1, 1, 1, 0}, {1, 1, 0, 0, 0} , {1, 2, 2, 2, 0} , {2, 1, 2, 0, 0}, {1, 2, 1, 1, 0}
-  , {2, 2, 0, 0, 0}, {2, 1, 0, 0, 0}, {2, 2, 2, 0, 0}, {1, 2, 2, 1, 0}, {2, 2, 1, 2, 0}, {1, 2, 1, 0, 0}
-  , {1, 1, 1, 0, 0}, {2, 0, 0, 0, 0}, {1, 1, 2, 0, 0}, {1, 1, 1, 2, 0}, {1, 2, 2, 0, 0}, {2, 1, 1, 2, 0}
-  , {2, 1, 2, 2, 0}, {2, 2, 1, 1, 0}
-  , {1, 2, 2, 2, 2}, {1, 1, 2, 2, 2}, {1, 1, 1, 2, 2}, {1, 1, 1, 1, 2}, {1, 1, 1, 1, 1}
-  , {2, 1, 1, 1, 1}, {2, 2, 1, 1, 1}, {2, 2, 2, 1, 1}, {2, 2, 2, 2, 1}, {2, 2, 2, 2, 2}
+// how many milliseconds to delay after each letter
+#define SPEED 300
+
+const int led = 4;
+enum { SHORT, LONG, OFF };
+
+const byte morse_dict[26][4] = {
+    { SHORT, LONG,  OFF,   OFF   }, // a
+    { LONG,  SHORT, SHORT, SHORT }, // b
+    { LONG,  SHORT, LONG,  SHORT }, // c
+    { LONG,  SHORT, SHORT, OFF   }, // d
+    { SHORT, OFF,   OFF,   OFF   }, // e
+    { SHORT, SHORT, LONG,  SHORT }, // f
+    { LONG,  LONG,  SHORT, OFF   }, // g
+    { SHORT, SHORT, SHORT, SHORT }, // h
+    { SHORT, SHORT, OFF,   OFF   }, // i
+    { SHORT, LONG,  LONG,  LONG  }, // j
+    { LONG,  SHORT, LONG,  OFF   }, // k
+    { SHORT, LONG,  SHORT, SHORT }, // l
+    { LONG,  LONG,  OFF,   OFF   }, // m
+    { LONG,  SHORT, OFF,   OFF   }, // n
+    { LONG,  LONG,  LONG,  OFF   }, // o
+    { SHORT, LONG,  LONG,  SHORT }, // p
+    { LONG,  LONG,  SHORT, LONG  }, // q
+    { SHORT, LONG,  SHORT, OFF   }, // r
+    { SHORT, SHORT, SHORT, OFF   }, // s
+    { LONG,  OFF,   OFF,   OFF   }, // t
+    { SHORT, SHORT, LONG,  OFF   }, // u
+    { SHORT, SHORT, SHORT, LONG  }, // v
+    { SHORT, LONG,  LONG,  OFF   }, // w
+    { LONG,  SHORT, SHORT, LONG  }, // x
+    { LONG,  SHORT, LONG,  LONG  }, // y
+    { LONG,  LONG,  SHORT, SHORT }, // z 
 };
 
-const char LETTERS [] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
-byte button = 0;
-unsigned long startTime = 0;
-unsigned long stopTime = 0;
-unsigned long time = 0;
+void blink_letter(const char letter) {
+    for (byte i = 0; i < 4; i++) {
+        switch (morse_dict[letter-97][i]) {
+            case SHORT:
+                digitalWrite(led, HIGH); delay(500);
+                digitalWrite(led, LOW);  delay(500 + SPEED);
+                break;
+            case LONG:
+                digitalWrite(led, HIGH); delay(750);
+                digitalWrite(led, LOW);  delay(750 + SPEED);
+                break;
+            case OFF:
+                digitalWrite(led, LOW);
+                break;
+        }
+    }
+}
 
-byte beepArray [] = {0, 0, 0, 0, 0};
-byte subIndex = 0;
-byte letterIndex = 0;
-char letter = '0';
-String str = "";
+void blink_string(const char *str) {
+    unsigned int i = 0;
 
-void setup() {
+    while (str[i++] != '\0') {
+        Serial.print(str[i]);
+        blink_letter(str[i]);
+    }
+    Serial.println();
+}
+
+void setup(void) {
   Serial.begin(9600);
-
-  pinMode(inPin, INPUT);
+  pinMode(led, OUTPUT);
 }
 
-byte letterSearch (byte array []) {
-  boolean match = 0;
-  for (byte index = 0; index < 36; index++ ) {
-    for (byte i = 0; i < 5; i++) {
-      match = 0;
-      if (array[i] != MORSECODE[index][i]) {
-        break;
-      }
-      match = 1;
-    }
-    if (match) return index;
-  }
-  return 255;
+void loop(void) {
+    blink_string(" HELLO");
+//    delay(2000);
 }
-
-void clearArray(byte array []) {
-  for (byte i = 0; i < 5; i++) {
-    array[i] = 0;
-  }
-}
-
-void printArray(byte array []) {
-  for (byte i = 0; i < 5; i++) {
-    Serial.print(array[i]);
-  }
-  Serial.println("");
-}
-
-
-void loop() {
-
-  startTime = millis();
-  while (button == HIGH) {
-    button = digitalRead(inPin);
-  }
-  stopTime = millis();
-  time = stopTime - startTime;
-
-  if (time > 20 && time < 300) {
-    beepArray[subIndex] = 1;
-    subIndex ++;
-  }
-  else if (time > 300 && time < 800) {
-    beepArray[subIndex] = 2;
-    subIndex ++;
-  }
-
-  startTime = millis();
-  while (button == LOW) {
-    button = digitalRead(inPin);
-  }
-  stopTime = millis();
-  time = stopTime - startTime;
-
-  if (time > 800) {
-    letterIndex = letterSearch(beepArray);
-    if (letterIndex != 255) {
-      letter = LETTERS[letterIndex];
-      str += letter;
-      Serial.print(letter);
-    }
-    clearArray(beepArray);
-    subIndex = 0;
-  }
-  if (time > 2000) {
-    str += " ";
-    Serial.print(" ");
-  }
-}
-
